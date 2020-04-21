7809a2dca294b53a28c4a05913935275fa6dc73e
unknown <mkarnadianas>
2020-04-21 22:13:06 +0800

    Memanggil metho dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===============");
+  System.out.println("NIDN  : 18630353 ");
+  System.out.println("Nama  : Adam Zorghy Ammar Wahidi ");
+  System.out.println("=================================");
+  System.out.println();
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mata Kuliah ===============");
+  System.out.println("NIDN  : TIF3402 ");
+  System.out.println("Nama  : Bahasa Indonesia ");
+  System.out.println("=================================");
+  System.out.println();
+ }
+}
